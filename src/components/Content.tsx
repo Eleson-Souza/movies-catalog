ede567d4db027a4fba19f3686c7b1180f62101a3
Eleson Souza <elesonsouza17@hotmail.com>
2022-06-02 20:53:24 -0300

    first commit

---
+++ src/components/Content.tsx
@@ -0,0 +1,41 @@
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  movies: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }[];
+  genreTitle: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.genreTitle}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
